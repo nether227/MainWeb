19f8190668c04c6479c2627fee7e6995827ff174
nether227 <109470193+nether227@users.noreply.github.com>
2022-07-17 21:02:40 +0300

    Initial commit

---
+++ src/main/java/com/example/mainweb/MainWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mainweb/MainWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mainweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
